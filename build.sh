d35047bb59569aeb1e449aeee53413eeb0dd5a9b
sngm45 <sng.ngmr@gmail.com>
2021-07-04 20:58:10 +0900

    update codepage update for MSYS2

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
 rm -rf ./build
 mkdir build
 cd build
+echo "Platform: $(uname -s)"
+if [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+cmake .. -G "MinGW Makefiles"
+mingw32-make
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW64_NT' ]; then
+cmake .. -G "MSYS Makefiles"
+mingw32-make
+else
 cmake ..
 make
+fi
 ./MyTest/MyTest
